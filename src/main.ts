635579f4ed40de69607c08f33c78a60d7f1d254c
Cyril POIDEVIN <vilsafur@gmail.com>
2020-12-22 19:37:27 +0100

    feat: initialize translation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Element from "./plugins/element";
+import { messages, defaultLocale } from "@/i18n";
+
+import { createI18n } from "vue-i18n";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: defaultLocale,
+  fallbackLocale: defaultLocale,
+  messages
+});
 
 import "normalize.css";
 
 createApp(App)
 .use(Element.ElementPlus, Element.options)
+.use(i18n)
 .mount("#app");
